7ac53f63c8bc92a2bf50e80003b890807fbdcf33
Douwe Drost <douwe.drostdefries@gmail.com>
2022-11-30 09:55:21 +0100

    Settings file added

---
+++ Core/Inc/settings.h
@@ -0,0 +1,13 @@
+/*
+ * settings.h
+ *
+ *  Created on: Nov 30, 2022
+ *      Author: douwe
+ */
+
+#ifndef INC_SETTINGS_H_
+#define INC_SETTINGS_H_
+
+#define buffersize 1000
+
+#endif /* INC_SETTINGS_H_ */
